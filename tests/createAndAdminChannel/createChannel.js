73b6e99e12995c2201ba505bf89ff085bcf05878
Edmont05 <101668511+Edmont05@users.noreply.github.com>
2024-10-04 19:07:39 -0400

    add v1

--- tests/createAndAdminChannel/createChannel.js
+++ tests/createAndAdminChannel/createChannel.js
@@ -1,55 +1,55 @@
-const { expect } = require('chai');
-const tags = require('mocha-tags');
+// const { expect } = require('chai');
+// const tags = require('mocha-tags');
 
-const testConfig = require('../../testConfig.json');
-const LoginPage = require('../../main/pages/loginPage');
-const { myBefore, myAfter, myAfterScreen } = require('../../main/hooks');
-const MainPage = require('../../main/pages/mainPage');
-const CreateForm = require('../../main/pages/createForm');
-const AddUserForm = require('../../main/pages/addUserForm');
-const SettingChannelForm = require('../../main/pages/SettingChannelForm');
+// const testConfig = require('../../testConfig.json');
+// const LoginPage = require('../../main/pages/loginPage');
+// const { myBefore, myAfter, myAfterScreen } = require('../../main/hooks');
+// const MainPage = require('../../main/pages/mainPage');
+// const CreateForm = require('../../main/pages/createForm');
+// const AddUserForm = require('../../main/pages/addUserForm');
+// const SettingChannelForm = require('../../main/pages/SettingChannelForm');
 
-describe('Test 1', function () {
-    this.timeout(50000);
-    before(async () => {
-        await myBefore();
-    });
+// describe('Test 1', function () {
+//     this.timeout(50000);
+//     before(async () => {
+//         await myBefore();
+//     });
 
-    afterEach(async function () {
-        await myAfterScreen.call(this);
-    });
+//     afterEach(async function () {
+//         await myAfterScreen.call(this);
+//     });
 
-    after(async () => {
-        // await MainPage.isVisible();
-        // await MainPage.clickRightNameChannel('edmont2');
-        // await MainPage.clickMoreInfoChannel();
+//     after(async () => {
+//         // await MainPage.isVisible();
+//         // await MainPage.clickRightNameChannel('edmont2');
+//         // await MainPage.clickMoreInfoChannel();
 
-        // await SettingChannelForm.isVisible();
-        // await SettingChannelForm.clickSettingTab();
-        // await SettingChannelForm.clickdeleteChannelButton();
-        // await SettingChannelForm.clickacceptedeleteChannelCheck();
-        // await SettingChannelForm.clickconfirmdeleteChannelButton();
-        await myAfter();
-    });
+//         // await SettingChannelForm.isVisible();
+//         // await SettingChannelForm.clickSettingTab();
+//         // await SettingChannelForm.clickdeleteChannelButton();
+//         // await SettingChannelForm.clickacceptedeleteChannelCheck();
+//         // await SettingChannelForm.clickconfirmdeleteChannelButton();
+//         await myAfter();
+//     });
 
-    tags('e2e').it('Test', async () => {
-        await LoginPage.isVisible();
-        await LoginPage.setCredentials(testConfig.credentials.username, testConfig.credentials.password);
-        await LoginPage.clickLoginButton();
-        await LoginPage.clickLinkSlack();
+//     tags('e2e').it('Test', async () => {
+//         await LoginPage.isVisible();
+//         await LoginPage.setCredentials(testConfig.credentials.username, testConfig.credentials.password);
+//         await LoginPage.clickLoginButton();
+//         await LoginPage.clickLinkSlack();
 
-        await MainPage.isVisible();
-        await MainPage.clickCreateNew();
-        await MainPage.clickOptionChannel();
+//         await MainPage.isVisible();
+//         await MainPage.clickCreateNew();
+//         await MainPage.clickOptionChannel();
 
-        await CreateForm.isVisible();
-        await CreateForm.setNameChannel("newChannel4");
-        await CreateForm.clickNext();
-        await CreateForm.clickPublicOption();
-        await CreateForm.clickCreate();
+//         await CreateForm.isVisible();
+//         await CreateForm.setNameChannel("newChannel4");
+//         await CreateForm.clickNext();
+//         await CreateForm.clickPublicOption();
+//         await CreateForm.clickCreate();
 
 
-        await AddUserForm.clickOmit();
+//         await AddUserForm.clickOmit();
 
-    })
-});
+//     })
+// });
