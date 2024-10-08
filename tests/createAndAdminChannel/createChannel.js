eaf0d2f61650b16049111ec7564edc3945cffcc3
Edmont05 <montanoeddy1120@gmail.com>
2024-10-08 15:33:11 -0400

    remove import setting channel

--- tests/createAndAdminChannel/createChannel.js
+++ tests/createAndAdminChannel/createChannel.js
@@ -7,7 +7,6 @@ const { myBefore, myAfter, myAfterScreen } = require("../../main/hooks");
 const MainPage = require("../../main/pages/mainPage");
 const CreateForm = require("../../main/pages/createForm");
 const AddUserForm = require("../../main/pages/addUserForm");
-const SettingChannelForm = require("../../main/pages/SettingChannelForm");
 
 describe("Test 1", function () {
   this.timeout(100000);
